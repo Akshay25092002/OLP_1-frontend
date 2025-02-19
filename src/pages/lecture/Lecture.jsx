72e74d73bd9375c59f17176ffc0497be29d7311c
Akshay Parmar <121486242+Akshay25092002@users.noreply.github.com>
2025-02-19 11:05:24 +0530

    Update Lecture.jsx

--- src/pages/lecture/Lecture.jsx
+++ src/pages/lecture/Lecture.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { server } from "../../main";
-import Loading from "../../components/loading/loading";
+import Loading from "../../components/loading/Loading";
 import "./lecture.css";
 import toast from "react-hot-toast";
 
